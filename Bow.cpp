717f1556109780518cfb0aec08cb2b096f2df62c
Andrew Fontaine <fontaineandrew@gmail.com>
2013-02-28 23:58:10 -0500

    Worked on bow use, TODO player death and choosing target

--- Bow.cpp
+++ Bow.cpp
@@ -1,9 +1,9 @@
 #include <string>
+#include <cstdlib>
+#include <iostream>
 #include "Player.h"
 #include "Item.h"
 #include "Bow.h"
-#include <random>
-#include <iostream>
 
 using namespace std;
 
@@ -22,4 +22,57 @@ int Bow::GetArrows() {
 	return this->Arrows;
 }
 
-void Bow::Use(Player *player);
+void Bow::Use(Player *player) {
+	Player *poorSucker;
+
+	cout << "Select the player you wish to use this bow on." << endl;
+	//TODO Add Player select
+
+	cout << "You are firing an arrow at " << poorSucker->GetName() << "." << endl;
+
+	int attack, defend, modify, loss;
+	int playerX, playerY;
+	int poorSuckerX, poorSuckerY;
+
+	playerX = player->GetLocation()->GetX();
+	playerY = player->GetLocation()->GetY();
+
+	poorSuckerX = poorSucker->GetLocation()->GetX();
+	poorSuckerY = poorSucker->GetLocation()->GetY();
+
+	attack = rand() % 20 + 1; // Roll d20s
+	defend = rand() % 20 + 1;
+	modify = sqrt((playerX*playerX - poorSuckerX*poorSuckerX) + (playerY*playerY + poorSuckerY*poorSuckerY)); // Cartesean distance
+	loss = (player->GetSkill()/modify); // Something I made up. Will probably alter later. Health loss
+
+	if(attack == 20 && defend == 1) { // attack: critical success; defend: critical failure
+		cout << poorSucker->GetName() << " got hit right in the eye socket! HEADSHOT!" << endl;
+		poorSucker->Dead(); //TODO implement Dead
+	}
+	else if(attack == 1 && defend == 20) { // attack: critical failure; defend: critical success
+		cout << poorSucker->GetName << " caught the arrow mid-flight! " << endl;
+		cout << "What's this? " << poorSucker->GetName() << " charged at " << player->GetName() << "!" << endl;
+		cout << poorSucker->GetName() << " sunk the arrow right through " << player->GetName() << "'s mouth!" << endl;
+		player->Dead(); // Seriously, we need to kill people
+	}
+	else if(defend == 20) // attack: unimportant; defend: critical success
+		cout << poorSucker->GetName << " grabbed the arrow mid-flight and snapped it in half!" << endl;
+	else if(loss < 0) // Check to see if there's acutal health loss
+		cout << "The arrow glanced off of " << poorSucker->GetName() << "'s mighty chest." << endl;
+	else if(attack == 1) { // attack: critical failure; defend: critical success
+		cout << player->GetName << "'s feeble attempt to fire a bow backfires and hits them in the foot!" << endl;
+		player->SetHealth(player->GetHealth() - 3);
+	}
+	else if(attack < defend) // attack rolls worse than defense
+		cout << poorSucker->GetName << " dodged the arrow! What a coward!" << endl;
+	else {
+		if (attack == 20) // attack: critical success; defense: unimportant
+			poorSucker->SetHealth(poorSucker->GetHealth() - loss*2); // double damage
+		else
+			poorSucker->SetHealth(poorSucker->GetHealth() - loss); // standard damage
+	}
+
+	this->Arrows--;
+
+	return;
+}
